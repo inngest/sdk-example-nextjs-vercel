6f1d4d612cb8962ebc3dde99f494ae595c8655b4
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-10-13 14:55:20 -0400

    Add second function

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { serve } from "inngest/next";
 import helloWorld from "../../inngest/helloWorld";
+import secondFn from "../../inngest/secondFn";
 
 export default async function middleware(
   req: NextApiRequest,
@@ -9,7 +10,7 @@ export default async function middleware(
   console.log("Environment variable: ", process.env["INNGEST_SIGNING_KEY"]);
   const stagingRegisterUrl = "https://api-dev.inngest.com/fn/register";
   console.log("Testing setting register URL to staging: ", stagingRegisterUrl);
-  const handler = serve("My Next.js App", [helloWorld], {
+  const handler = serve("My Next.js App", [helloWorld, secondFn], {
     inngestRegisterUrl: stagingRegisterUrl,
   });
   return await handler(req, res);
