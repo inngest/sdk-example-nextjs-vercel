7da7939719574191efda9cec99ff99e89d1113aa
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-10-13 14:48:29 -0400

    Set staging URL for testing

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,4 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { serve } from "inngest/next";
 import helloWorld from "../../inngest/helloWorld";
 
-export default serve("My Next.js App", [helloWorld]);
+export default async function middleware(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("Environment variable: ", process.env["INNGEST_SIGNING_KEY"]);
+  const stagingRegisterUrl = "https://api-dev.inngest.com/fn/register";
+  console.log("Testing setting register URL to staging: ", stagingRegisterUrl);
+  const handler = serve("My Next.js App", [helloWorld], {
+    inngestRegisterUrl: stagingRegisterUrl,
+  });
+  return await handler(req, res);
+}
